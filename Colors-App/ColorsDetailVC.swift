ce1ec5217663aacfdb4e236375228f0c55a26006
bahittinn <bahittin84@gmail.com>
2023-07-17 10:00:19 +0300

    Details Added

--- Colors-App/ColorsDetailVC.swift
+++ Colors-App/ColorsDetailVC.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class ColorsDetailVC: UIViewController {
-
+    var backcolor: UIColor?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(backcolor)
+        view.backgroundColor = backcolor
         // Do any additional setup after loading the view.
     }
     
--- Colors-App/ColorsTableVC.swift
+++ Colors-App/ColorsTableVC.swift
@@ -8,24 +8,48 @@
 import UIKit
 
 class ColorsTableVC: UIViewController {
-    
+    var colors: [UIColor] = []
+    var selectedColor: UIColor?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addRandomColors()
     }
-   
+    
+    func addRandomColors()  {
+        for _ in 0..<50 {
+            colors.append(createRandomColor())
+        }
+    }
+    
+    func createRandomColor() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in: 0...1),
+                                  green: CGFloat.random(in: 0...1),
+                                  blue: CGFloat.random(in: 0...1),
+                                  alpha: 1)
+        return randomColor
+    }
+    
+    
 }
 
 extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 50
+        return colors.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = UITableViewCell()
-         cell.textLabel?.text = "deneme"
-         return cell
-        //returns blank table view cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCell",for: indexPath)
+        let color = colors[indexPath.row]
+        cell.backgroundColor = color
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedColor = colors[indexPath.row]
+        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! ColorsDetailVC
+        destination.backcolor = selectedColor
     }
 }
